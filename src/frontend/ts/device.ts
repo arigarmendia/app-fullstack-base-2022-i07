7066496a9d6ccb9d3e4eb44feec9d9c79eb27782
Ariadna Garmendia <ari@Ariadnas-MBP.attlocal.net>
2022-12-06 14:06:44 -0600

    primer commit con MySQL andando

---
+++ src/frontend/ts/device.ts
@@ -0,0 +1,8 @@
+class Device{
+    public id: number;
+    public name: string;
+    public state: boolean;
+    public description: string;
+    public type: number;
+
+}
--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,104 @@
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+class Main implements EventListenerObject,HandleResponse{
+ 
+    private framework: Framework = new Framework();
+   
+    cosultarDispositivoAlServidor() {
+
+        this.framework.ejecutarRequest("GET", "http://localhost:8000/devices",this);
+    }
+
+
+    cambiarEstadoDispositivoAlServidor() {
+        let json = { id: 1, state: 0 };
+        this.framework.ejecutarRequest("POST", "http://localhost:8000/deviceChange",this,json);
+        
+    }
+    cargarGrilla(listaDisp: Array<Device>) {
+        console.log("llego info del servidor", listaDisp);    
+        let cajaDips = document.getElementById("cajaDisp");
+        let grilla:string = "<ul class='collection'>";
+        for (let disp of listaDisp) {
+        
+
+            grilla += ` <li class="collection-item avatar">`;
+            
+            if (disp.type == 1) {
+                grilla+=`<img src="static/images/lightbulb.png" alt="" class="circle"> `   
+            } else {
+                grilla+=`<img src="static/images/window.png" alt="" class="circle"> `  
+            }
+            
+            grilla += ` <span class="title negrita">${disp.name}</span>
+            <p>${disp.description}
+            </p>
+            <a href="#!" class="secondary-content">
+              <div class="switch">
+                  <label>
+                    Off`;
+            if (disp.state) {
+                grilla += `<input id="cb_${disp.id}" type="checkbox" checked>`;    
+            } else {
+                grilla += `<input id="cb_${disp.id}" type="checkbox">`;    
+            }
+            
+            
+            grilla +=`<span class="lever"></span>
+                    On
+                  </label>
+                </div>
+          </a>
+          </li>`;
+        }
+        grilla += "</ul>"
+        
+        cajaDips.innerHTML = grilla;
+
+        for (let disp of listaDisp) {
+            let cb = document.getElementById("cb_" + disp.id);
+            cb.addEventListener("click", this);
+        }
+        
+        this.framework.ocultarCargando();
+        
+    }
+
+    handleEvent(object: Event): void {
+     
+        let tipoEvento:string=object.type;
+        let objEvento: HTMLElement;
+        objEvento = <HTMLElement>object.target;
+        if(objEvento.id=="btnOtro"){
+            console.log(objEvento.id, objEvento.textContent); 
+            
+            let iNombre =<HTMLInputElement> document.getElementById("iNombre");
+            
+            objEvento.textContent = iNombre.value;
+            alert("hola estoy en el main");
+        } else if (objEvento.id == "btnSaludar") {
+          
+            this.framework.mostrarCargando();
+            this.cosultarDispositivoAlServidor();
+
+      
+        } else if (objEvento.id.startsWith("cb_")) {
+            let idDisp = objEvento.id.substring(3);
+            
+            alert("Se cambio el estado del dispositivo " + idDisp + " -" + (<HTMLInputElement>objEvento).checked);
+
+       
+            
+        }
+
+    }
 }
+
+
+window.addEventListener("load", () => {
+    let main: Main = new Main();
+    main.cosultarDispositivoAlServidor();
+
+});
+
+
+
